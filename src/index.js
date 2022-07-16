17965a281f5bdbc32a417229f13ada15a172fe0c
falihnurandino <andinofalih1@gmail.com>
2022-07-16 22:20:27 +0700

    fix: import and component render

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import Header from './components/header';
+import NotesApp from './components/NotesApp';
 
 // import style
 import './styles/style.css';
 
 const root = createRoot(document.getElementById('root'));
-root.render(<Header/>);
+root.render(<NotesApp/>);

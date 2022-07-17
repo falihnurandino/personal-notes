648e13894a0eb8ec5fc7493b4b62901b2ad65d4f
falihnurandino <andinofalih1@gmail.com>
2022-07-17 17:12:33 +0700

    add atribute required to input and textarea

--- src/components/NotesInput.js
+++ src/components/NotesInput.js
@@ -4,8 +4,8 @@ function NotesInput() {
     return (
         <div className='notes-app__input'>
             <form>
-                <input type='text' placeholder='Title'/>
-                <textarea type='text' placeholder='Write your note'></textarea>
+                <input type='text' placeholder='Title' required/>
+                <textarea type='text' placeholder='Write your note' required></textarea>
                 <button type='submit' >Create</button>
             </form>
         </div>

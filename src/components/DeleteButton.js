a10c11daeca3099be5561508d682769ccd137b5f
falihnurandino <andinofalih1@gmail.com>
2022-08-02 18:39:31 +0700

    add delete button

---
+++ src/components/DeleteButton.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+function DeleteButton ({ onClick }) {
+    return (
+        <button className="danger" onClick={onClick}>Delete</button>
+    );
+}
+
+export default DeleteButton;

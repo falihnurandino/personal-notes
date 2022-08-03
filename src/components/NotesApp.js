e830ae9e5aa1ed00b0eaa8d2bebe63072689b398
falihnurandino <andinofalih1@gmail.com>
2022-08-03 15:14:24 +0700

    add component NotesList

--- src/components/NotesApp.js
+++ src/components/NotesApp.js
@@ -1,8 +1,51 @@
 import React from 'react';
 import NotesHeader from './NotesHeader';
 import NotesInput from './NotesInput';
+import { getInitialData } from '../utils';
+import NotesList from './NotesList';
 
 class NotesApp extends React.Component {
+    constructor() {
+        super();
+
+        this.state = {
+            searchQuery: "",
+            notes: getInitialData()
+        };
+
+        this.deleteNoteHandler = this.deleteNotesHandler.bind(this);
+        this.archiveNoteHandler = this.archiveNotesHandler.bind(this);
+    }
+
+    deleteNotesHandler(id) {
+        this.setState((prevState) => {
+            return {
+                ...prevState,
+                notes: prevState.notes.filter((note) => note.id !== id),
+            };
+        });
+    }
+
+    archiveNotesHandler(id) {
+        const { notes } = this.state;
+        notes.forEach((note) => {
+            if (note.id === id) note.archived = !note.archived;
+        });
+        this.setState((prevState) => ({ ...prevState, notes }));
+    }
+
+    notesList() {
+        const { searchQuery, notes } = this.state;
+
+        const list = searchQuery.trim().length ? notes.filter((note) => 
+        note.title.toLowerCase().includes(searchQuery.toLowerCase())
+        )
+        : notes;
+
+        return list.sort((a, b) => a.date - b.date).reverse();
+    }
+
+
     render() {
         return (
             <div className='notes-app'>
@@ -11,7 +54,22 @@ class NotesApp extends React.Component {
                 <div className='notes-app__main'>
                 <h2>Create a note</h2>
                 <NotesInput/>
+
+                <h2>Notes</h2>
+                <NotesList 
+                list={this.notesList().filter((note) => !note.archived)}
+                deleteNote={this.deleteNotesHandler}
+                archiveNote={this.archiveNotesHandler}
+                />
+
+                <h2>Archived Notes</h2>
+                <NotesList 
+                list={this.notesList().filter((note) => note.archived)}
+                deleteNote={this.deleteNotesHandler}
+                archiveNote={this.archiveNotesHandler}
+                />
                 </div>
+
             </div>
         )
     }

6633ab5c9042a5cd1f5a1b96b103fba0377e189f
falihnurandino <andinofalih1@gmail.com>
2022-08-03 14:32:59 +0700

    create notes item body

---
+++ src/components/NotesItemBody.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { showFormattedDate } from "../utils";
+
+function NotesItemBody ({ title, createdAt, body }) {
+    return (
+        <div className='notes__item-body'>
+            <h3>{title}</h3>
+            <time dateTime={createdAt}>{showFormattedDate(createdAt)}</time>
+            <p>{body}</p>
+        </div>
+    );
+}
+
+export default NotesItemBody;

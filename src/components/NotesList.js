070689d47c744a043e0eb593d908a5e90d5d8b9d
falihnurandino <andinofalih1@gmail.com>
2022-09-13 12:41:23 +0700

    add property validation to NotesList

--- src/components/NotesList.js
+++ src/components/NotesList.js
@@ -1,23 +1,30 @@
-import React from "react";
-import NotesItem from "./NotesItem";
+import React from 'react';
+import PropTypes from 'prop-types';
+import NotesItem from './NotesItem';
 
-function NotesList ({ list, deleteNote, archiveNote}) {
-    if (list.length) {
-        return (
-            <div className="notes-list">
-                {list.map((item) => (
-                    <NotesItem
-                        note={item}
-                        key={item.id}
-                        deleteNote={deleteNote}
-                        archiveNote={archiveNote}
-                    />
-                ))}
-            </div>
-        );
-    }
+function NotesList({ list, deleteNote, archiveNote }) {
+  if (list.length) {
+    return (
+      <div className="notes-list">
+        {list.map((item) => (
+          <NotesItem
+            note={item}
+            key={item.id}
+            deleteNote={deleteNote}
+            archiveNote={archiveNote}
+          />
+        ))}
+      </div>
+    );
+  }
 
-    return <p>Tidak ada catatan</p>;
-}    
+  return <p>Tidak ada catatan</p>;
+}
+
+NotesList.propTypes = {
+  list: PropTypes.arrayOf(PropTypes.object).isRequired,
+  deleteNote: PropTypes.func.isRequired,
+  archiveNote: PropTypes.func.isRequired,
+};
 
 export default NotesList;

56b7fd5099ee9a4e68f14e16cf3cb1ffc62fc28b
falihnurandino <andinofalih1@gmail.com>
2022-09-13 12:42:43 +0700

    add property validation to DeleteButton

--- src/components/DeleteButton.js
+++ src/components/DeleteButton.js
@@ -1,9 +1,16 @@
-import React from "react";
+import React from 'react';
+import PropTypes from 'prop-types';
 
-function DeleteButton ({ onClick }) {
-    return (
-        <button className="delete-button" onClick={onClick}>Delete</button>
-    );
+function DeleteButton({ onClick }) {
+  return (
+    <button className="delete-button" onClick={onClick}>
+      Delete
+    </button>
+  );
 }
+
+DeleteButton.propTypes = {
+  onClick: PropTypes.func.isRequired,
+};
 
 export default DeleteButton;

3b2f7126650d5745f18aba68ad3865d9e473e1ee
falihnurandino <andinofalih1@gmail.com>
2022-09-13 12:42:26 +0700

    add property validation to NotesInput

--- src/components/NotesInput.js
+++ src/components/NotesInput.js
@@ -1,73 +1,77 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 class NotesInput extends React.Component {
-    constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props);
 
-        this.state = {
-            title: "",
-            body: ""
-        };
-    
+    this.state = {
+      title: '',
+      body: '',
+    };
 
     this.onTitleChangeListener = this.onTitleChangeListener.bind(this);
     this.onBodyChangeListener = this.onBodyChangeListener.bind(this);
     this.onSubmitListener = this.onSubmitListener.bind(this);
-    }
+  }
 
-    onTitleChangeListener(event) {
-        const { value } = event.target;
+  onTitleChangeListener(event) {
+    const { value } = event.target;
 
-        this.setState((prevState) => {
-            return {
-                ...prevState,
-                title: value.length > 50 ? value.slice(0, 50) : value,
-            };
-        });
-    }
+    this.setState((prevState) => {
+      return {
+        ...prevState,
+        title: value.length > 50 ? value.slice(0, 50) : value,
+      };
+    });
+  }
 
-    onBodyChangeListener(event) {
-       this.setState((prevState) => {
-            return {
-                ...prevState,
-                body: event.target.value,
-            };
-        });
-    }
+  onBodyChangeListener(event) {
+    this.setState((prevState) => {
+      return {
+        ...prevState,
+        body: event.target.value,
+      };
+    });
+  }
 
-    onSubmitListener(event) {
-        event.preventDefault();
-        this.props.addNote(this.state);
-        this.setState({ title: "", body: "" });
-    }
+  onSubmitListener(event) {
+    event.preventDefault();
+    this.props.addNote(this.state);
+    this.setState({ title: '', body: '' });
+  }
 
-    render() {
-        const { title, body } = this.state;
-    
+  render() {
+    const { title, body } = this.state;
+
     return (
-        <div className="notes-app__input">
-            <form onSubmit={this.onSubmitListener}>
-                <p>{title.length}/50</p>
-                <input 
-                type='text' 
-                placeholder='Title'
-                onChange={this.onTitleChangeListener}
-                value={title} 
-                required
-                />
-                
-                <textarea 
-                type='text' 
-                placeholder='Write your note'
-                onChange={this.onBodyChangeListener}
-                value={body} 
-                required
-                />
-                <button type='submit' >Create</button>
-            </form>
-        </div>
-        )
-    }
+      <div className="notes-app__input">
+        <form onSubmit={this.onSubmitListener}>
+          <p>{title.length}/50</p>
+          <input
+            type="text"
+            placeholder="Title"
+            onChange={this.onTitleChangeListener}
+            value={title}
+            required
+          />
+
+          <textarea
+            type="text"
+            placeholder="Write your note"
+            onChange={this.onBodyChangeListener}
+            value={body}
+            required
+          />
+          <button type="submit">Create</button>
+        </form>
+      </div>
+    );
+  }
 }
+
+NotesInput.propTypes = {
+  addNote: PropTypes.func.isRequired,
+};
 
 export default NotesInput;

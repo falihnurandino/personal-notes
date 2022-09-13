5b20112b3f2a6072a49eb3850ca324cd8a8c19aa
falihnurandino <andinofalih1@gmail.com>
2022-09-13 12:42:05 +0700

    add property validation to NotesItem

--- src/components/NotesItem.js
+++ src/components/NotesItem.js
@@ -1,27 +1,33 @@
-import React from "react";
-import DeleteButton from "./DeleteButton";
-import ArchiveButton from "./ArchiveButton";
-import NotesItemBody from "./NotesItemBody";
+import React from 'react';
+import PropTypes from 'prop-types';
+import DeleteButton from './DeleteButton';
+import ArchiveButton from './ArchiveButton';
+import NotesItemBody from './NotesItemBody';
 
-function NotesItem ({ note, deleteNote, archiveNote }) {
-    return (
-        <div className="notes-item">
-            <NotesItemBody 
-            title={note.title} 
-            createdAt={note.createdAt} 
-            body={note.body}
-            />
+function NotesItem({ note, deleteNote, archiveNote }) {
+  return (
+    <div className="notes-item">
+      <NotesItemBody
+        title={note.title}
+        createdAt={note.createdAt}
+        body={note.body}
+      />
 
-        <div className="notes-item__actions">
-            <DeleteButton onClick={() => deleteNote(note.id)}/>
-            <ArchiveButton 
-            archived={note.archived} 
-            onClick={() => archiveNote(note.id)}
-            />
-
-        </div>
-        </div>
-    );
+      <div className="notes-item__actions">
+        <DeleteButton onClick={() => deleteNote(note.id)} />
+        <ArchiveButton
+          archived={note.archived}
+          onClick={() => archiveNote(note.id)}
+        />
+      </div>
+    </div>
+  );
 }
+
+NotesItem.propTypes = {
+  note: PropTypes.object.isRequired,
+  deleteNote: PropTypes.func.isRequired,
+  archiveNote: PropTypes.func.isRequired,
+};
 
 export default NotesItem;

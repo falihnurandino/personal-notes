f5edf5f594d1dadbe8dfe5c95f4327e3a75f34e5
falihnurandino <andinofalih1@gmail.com>
2022-09-13 12:43:01 +0700

    add property validation to ArchiveButton

--- src/components/ArchiveButton.js
+++ src/components/ArchiveButton.js
@@ -1,11 +1,17 @@
-import React from "react";
+import React from 'react';
+import PropTypes from 'prop-types';
 
-function ArchiveButton ({ archived, onClick }) {
-    return (
-        <button className="archive-button" onClick={onClick}>
-            {archived ? "Move" : "Archived"}
-        </button>
-    );
+function ArchiveButton({ archived, onClick }) {
+  return (
+    <button className="archive-button" onClick={onClick}>
+      {archived ? 'Move' : 'Archived'}
+    </button>
+  );
 }
+
+ArchiveButton.propTypes = {
+  archived: PropTypes.bool.isRequired,
+  onClick: PropTypes.func.isRequired,
+};
 
 export default ArchiveButton;

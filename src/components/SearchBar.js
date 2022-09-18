1490413461419b7ae15490554af02c1830bcb84d
falihnurandino <andinofalih1@gmail.com>
2022-09-18 18:44:29 +0700

    refactor SearchBar

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,16 +1,22 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-function SearchBar({ onTyping }) {
+function SearchBar({ keyword, keywordChange }) {
   return (
     <div className="search-bar">
-      <input type="text" placeholder="Search..." onChange={onTyping} />
+      <input
+        type="text"
+        placeholder="Search..."
+        value={keyword}
+        onChange={(event) => keywordChange(event.target.value)}
+      />
     </div>
   );
 }
 
 SearchBar.propTypes = {
-  onTyping: PropTypes.func.isRequired,
+  keywordChange: PropTypes.func.isRequired,
+  keyword: PropTypes.string.isRequired,
 };
 
 export default SearchBar;

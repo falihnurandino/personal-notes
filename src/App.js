fa3acd351c64ff5e27b8f26d8aaad197734aa2ec
falihnurandino <andinofalih1@gmail.com>
2022-09-18 18:44:02 +0700

    refactor App.js

--- src/App.js
+++ src/App.js
@@ -1,106 +1,30 @@
 import React from 'react';
-import NotesHeader from './components/NotesHeader';
-import NotesInput from './components/NotesInput';
-import NotesList from './components/NotesList';
-import { getAllNotes } from './utils/local-data';
+import { Routes, Route, Link } from 'react-router-dom';
+import Navigation from './components/Navigation';
+import AddPage from './pages/AddPage';
+import ArchivesNotesPage from './pages/ArchivesNotesPage';
+import DetailNotesPage from './pages/DetailNotesPage';
+import HomePage from './pages/HomePage';
 
-class App extends React.Component {
-  constructor() {
-    super();
-
-    this.state = {
-      searchQuery: '',
-      notes: getAllNotes(),
-    };
-
-    this.addNotesHandler = this.addNotesHandler.bind(this);
-    this.deleteNotesHandler = this.deleteNotesHandler.bind(this);
-    this.archiveNotesHandler = this.archiveNotesHandler.bind(this);
-    this.searchTypingHandler = this.searchTypingHandler.bind(this);
-  }
-
-  addNotesHandler({ title, body }) {
-    this.setState((prevState) => {
-      return {
-        ...prevState,
-        notes: [
-          ...prevState.notes,
-          {
-            id: +new Date(),
-            title,
-            body,
-            createdAt: new Date().toISOString(),
-            archived: false,
-          },
-        ],
-      };
-    });
-  }
-
-  searchTypingHandler(event) {
-    this.setState((prevState) => {
-      return {
-        ...prevState,
-        searchQuery: event.target.value,
-      };
-    });
-  }
-
-  deleteNotesHandler(id) {
-    this.setState((prevState) => {
-      return {
-        ...prevState,
-        notes: prevState.notes.filter((note) => note.id !== id),
-      };
-    });
-  }
-
-  archiveNotesHandler(id) {
-    const { notes } = this.state;
-    notes.forEach((note) => {
-      if (note.id === id) note.archived = !note.archived;
-    });
-    this.setState((prevState) => ({ ...prevState, notes }));
-  }
-
-  notesList() {
-    const { searchQuery, notes } = this.state;
-
-    const list = searchQuery.trim().length
-      ? notes.filter((note) =>
-          note.title.toLowerCase().includes(searchQuery.toLowerCase())
-        )
-      : notes;
-
-    return list.sort((a, b) => a.date - b.date).reverse();
-  }
-
-  render() {
-    return (
-      <div className="notes-app">
-        <NotesHeader onTyping={this.searchTypingHandler} />
-
-        <div className="notes-app__main">
-          <h2 className="notes-app__input-title">Create a note</h2>
-          <NotesInput addNote={this.addNotesHandler} />
-
-          <h2 className="notes-app__list-title">Active Notes</h2>
-          <NotesList
-            list={this.notesList().filter((note) => !note.archived)}
-            deleteNote={this.deleteNotesHandler}
-            archiveNote={this.archiveNotesHandler}
-          />
-
-          <h2 className="notes-app__archived-title">Archived Notes</h2>
-          <NotesList
-            list={this.notesList().filter((note) => note.archived)}
-            deleteNote={this.deleteNotesHandler}
-            archiveNote={this.archiveNotesHandler}
-          />
-        </div>
-      </div>
-    );
-  }
+function App() {
+  return (
+    <div className="notes-app">
+      <header className="notes-app__header">
+        <h1>
+          <Link to="/">Notes App</Link>
+        </h1>
+        <Navigation />
+      </header>
+      <main>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/notes/:id" element={<DetailNotesPage />} />
+          <Route path="/archives" element={<ArchivesNotesPage />} />
+          <Route path="/notes/new" element={<AddPage />} />
+        </Routes>
+      </main>
+    </div>
+  );
 }
 
 export default App;

55d255a4aa3b1171ec89d24f2f5e78c2affe6d6f
falihnurandino <andinofalih1@gmail.com>
2022-09-27 20:10:35 +0700

    create AddPage

---
+++ src/pages/AddPage.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import NotesInput from '../components/NotesInput';
+import { addNote } from '../utils/local-data';
+
+export default function AddPage() {
+  const navigate = useNavigate();
+
+  function onAddNoteHandler(note) {
+    addNote(note);
+    navigate('/');
+  }
+  return (
+    <section>
+      <NotesInput addNote={onAddNoteHandler} />
+    </section>
+  );
+}

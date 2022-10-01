c8f796572955f073402610fd836d686ebb824c67
falihnurandino <andinofalih1@gmail.com>
2022-10-01 17:47:07 +0700

    refactor NotesItem

--- src/components/NotesItem.js
+++ src/components/NotesItem.js
@@ -1,33 +1,23 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import DeleteButton from './DeleteButton';
-import ArchiveButton from './ArchiveButton';
-import NotesItemBody from './NotesItemBody';
+import showFormattedDate from '../utils';
+import { Link } from 'react-router-dom';
 
-function NotesItem({ note, deleteNote, archiveNote }) {
+export default function NotesItem({ note }) {
+  const { id, title, body, createdAt } = note;
+  const formattedDate = showFormattedDate(createdAt);
+
   return (
     <div className="notes-item">
-      <NotesItemBody
-        title={note.title}
-        createdAt={note.createdAt}
-        body={note.body}
-      />
-
-      <div className="notes-item__actions">
-        <DeleteButton onClick={() => deleteNote(note.id)} />
-        <ArchiveButton
-          archived={note.archived}
-          onClick={() => archiveNote(note.id)}
-        />
-      </div>
+      <h3 className="notes-item__title">
+        <Link to={`/notes/${id}`}>{title}</Link>
+      </h3>
+      <time className="notes-item__createdAt">{formattedDate}</time>
+      <p className="notes-item__body">{body}</p>
     </div>
   );
 }
 
 NotesItem.propTypes = {
   note: PropTypes.object.isRequired,
-  deleteNote: PropTypes.func.isRequired,
-  archiveNote: PropTypes.func.isRequired,
 };
-
-export default NotesItem;

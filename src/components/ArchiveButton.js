fe8d2a42ccbfaec4a5a470c372a57e760972173a
falihnurandino <andinofalih1@gmail.com>
2022-10-01 17:45:34 +0700

    refactor ArchiveButton

--- src/components/ArchiveButton.js
+++ src/components/ArchiveButton.js
@@ -1,17 +1,30 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { FaArchive } from 'react-icons/fa';
 
-function ArchiveButton({ archived, onClick }) {
+export default function ArchiveButton({
+  id,
+  archived,
+  onArchive,
+  onUnArchive,
+}) {
   return (
-    <button className="archive-button" onClick={onClick}>
-      {archived ? 'Move' : 'Archived'}
+    <button
+      className="action"
+      onClick={() => (archived ? onUnArchive(id) : onArchive(id))}
+    >
+      {archived ? (
+        <FaArchive className="action" fill="#fff" />
+      ) : (
+        <FaArchive className="action" />
+      )}
     </button>
   );
 }
 
 ArchiveButton.propTypes = {
+  id: PropTypes.string.isRequired,
   archived: PropTypes.bool.isRequired,
-  onClick: PropTypes.func.isRequired,
+  onArchive: PropTypes.func.isRequired,
+  onUnArchive: PropTypes.func.isRequired,
 };
-
-export default ArchiveButton;

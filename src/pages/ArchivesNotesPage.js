bbdb385796485aaf8a700e74b64bd15b509bb8c8
falihnurandino <andinofalih1@gmail.com>
2022-10-01 17:43:24 +0700

    create ArchivesNotesPage

---
+++ src/pages/ArchivesNotesPage.js
@@ -0,0 +1,63 @@
+import React from 'react';
+import { useSearchParams } from 'react-router-dom';
+import NotesList from '../components/NotesList';
+import SearchBar from '../components/SearchBar';
+import { getArchivedNotes } from '../utils/local-data';
+
+export default function ArchivesNotesPageWrapper() {
+  const [searchParams, setSearchParams] = useSearchParams();
+  const keyword = searchParams.get('keyword');
+
+  function changeSearchParams(keyword) {
+    setSearchParams({ keyword });
+  }
+
+  return (
+    <ArchivesNotesPage
+      defaultKeyword={keyword}
+      keywordChange={changeSearchParams}
+    />
+  );
+}
+
+class ArchivesNotesPage extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      keyword: props.defaultKeyword || '',
+      notes: getArchivedNotes(),
+    };
+
+    this.onKeywordChangeHandler = this.onKeywordChangeHandler.bind(this);
+  }
+
+  onKeywordChangeHandler(keyword) {
+    this.setState(() => {
+      return {
+        keyword,
+      };
+    });
+
+    this.props.keywordChange(keyword);
+  }
+
+  render() {
+    const notes = this.state.notes.filter((note) => {
+      return note.title
+        .toLowerCase()
+        .includes(this.state.keyword.toLowerCase());
+    });
+
+    return (
+      <section>
+        <h2>Archive Notes</h2>
+        <SearchBar
+          keyword={this.state.keyword}
+          keywordChange={this.onKeywordChangeHandler}
+        />
+        <NotesList notes={notes} />
+      </section>
+    );
+  }
+}

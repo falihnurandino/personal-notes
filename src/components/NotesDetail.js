ae653bc33370c292f0d99f18a6ac6a9a28146016
falihnurandino <andinofalih1@gmail.com>
2022-10-01 20:06:43 +0700

    import and add parser to body

--- src/components/NotesDetail.js
+++ src/components/NotesDetail.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import parser from 'html-react-parser';
 import ArchiveButton from './ArchiveButton';
 import DeleteButton from './DeleteButton';
 import showFormattedDate from '../utils';
@@ -16,7 +17,7 @@ export default function NotesDetail({
     <section className="detail-page">
       <h3 className="detail-page__title">{title}</h3>
       <time className="detail-page__createdAt"> {formattedDate}</time>
-      <div className="detail-page__body">{body}</div>
+      <div className="detail-page__body">{parser(body)}</div>
       <div className="detail-page__action">
         <ArchiveButton
           id={id}

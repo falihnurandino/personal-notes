46a4c90717dd68ae22ab8889b6027fc7485a6675
falihnurandino <andinofalih1@gmail.com>
2022-10-01 20:06:11 +0700

    import and add parser to body

--- src/components/NotesItem.js
+++ src/components/NotesItem.js
@@ -2,18 +2,19 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import showFormattedDate from '../utils';
 import { Link } from 'react-router-dom';
+import parser from 'html-react-parser';
 
 export default function NotesItem({ note }) {
   const { id, title, body, createdAt } = note;
   const formattedDate = showFormattedDate(createdAt);
 
   return (
-    <div className="notes-item">
+    <div className="notes-item" tabIndex="0">
       <h3 className="notes-item__title">
         <Link to={`/notes/${id}`}>{title}</Link>
       </h3>
       <time className="notes-item__createdAt">{formattedDate}</time>
-      <p className="notes-item__body">{body}</p>
+      <p className="notes-item__body">{parser(body)}</p>
     </div>
   );
 }

3603cf550a0a7382941f9e436e0a26d78e114a71
falihnurandino <andinofalih1@gmail.com>
2022-10-06 19:59:46 +0700

    create Loader

---
+++ src/components/Loader.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+export default function Loader() {
+  return (
+    <div
+      className="loader-wrapper"
+      style={{
+        display: 'grid',
+        alignItems: 'center',
+        justifyContent: 'center',
+        height: '400px',
+        marginTop: '1em',
+      }}
+    >
+      <div className="lmns lmns-loader three-wavy-balls">
+        <span />
+        <span />
+        <span />
+      </div>
+    </div>
+  );
+}

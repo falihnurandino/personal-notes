fbe24c65f6328c28ae6fef0f0bef98e66a02c7ce
falihnurandino <andinofalih1@gmail.com>
2022-10-08 10:38:05 +0700

    import LocaleContext

--- src/components/NotesList.js
+++ src/components/NotesList.js
@@ -1,22 +1,28 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import NotesItem from './NotesItem';
+import LocaleContext from '../contexts/LocaleContext';
 
 export default function NotesList({ notes }) {
-  if (notes.length) {
-    return (
-      <div className="notes-list">
-        {notes.map((note) => (
-          <NotesItem key={note.id} id={note.id} note={note} />
-        ))}
-      </div>
-    );
-  }
+  const { locale } = React.useContext(LocaleContext);
 
   return (
-    <div className="notes-list-empty not-found">
-      <h2>No Notes Found</h2>
-    </div>
+    <>
+      {notes.length ? (
+        <div className="notes-list">
+          {notes.map((note) => (
+            <NotesItem key={note.id} id={note.id} note={note} />
+          ))}
+        </div>
+      ) : (
+        <div className="notes-list-empty not-found">
+          <h2>
+            {locale === 'en' ? 'No Notes Found' : 'Catatan Tidak Ditemukan'}
+          </h2>
+        </div>
+      )}
+      ;
+    </>
   );
 }
 

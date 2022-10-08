8585fade88242c2bbb31a98fcb9cf21c6509e011
falihnurandino <andinofalih1@gmail.com>
2022-10-08 10:39:45 +0700

    import LocaleContext to searchBar

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import LocaleContext from '../contexts/LocaleContext';
 
 export default function SearchBar({ keyword, keywordChange }) {
+  const { locale } = React.useContext(LocaleContext);
   return (
     <div className="search-bar" tabIndex="0">
       <input
         type="text"
-        placeholder="Search..."
+        placeholder={
+          locale === 'en' ? 'Search by title...' : 'Cari berdasarkan judul...'
+        }
         value={keyword}
         onChange={(event) => keywordChange(event.target.value)}
       />
